95163808be8d10fd4ccfd3589e044eda206ed019
lukunpeng <lukunpeng@aiyingli.com>
2019-11-05 10:37:31 +0800

    默认显示插槽内容更为合理

--- packages/alert/src/main.vue
+++ packages/alert/src/main.vue
@@ -11,8 +11,9 @@
         <span class="el-alert__title" :class="[ isBoldTitle ]" v-if="title || $slots.title">
           <slot name="title">{{ title }}</slot>
         </span>
-        <p class="el-alert__description" v-if="$slots.default && !description"><slot></slot></p>
-        <p class="el-alert__description" v-if="description && !$slots.default">{{ description }}</p>
+        <p class="el-alert__description">
+          <slot>{{ description }}</slot>
+        </p>
         <i class="el-alert__closebtn" :class="{ 'is-customed': closeText !== '', 'el-icon-close': closeText === '' }" v-show="closable" @click="close()">{{closeText}}</i>
       </div>
     </div>
